6b03990663497f075055fecd81f02b45867aa1e1
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-07-27 22:44:49 -0700

    improvement: misc tidying and refactoring

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -7,13 +7,33 @@ use yew::prelude::*;
 #[function_component]
 fn App() -> Html {
     html! {
-        <div>
-            <h1 class="text-3xl">{ "Welcome to Yew!"}</h1>
-        </div>
+        <>
+            <header>
+                <h1>{ "Hello Yew!" }</h1>
+            </header>
+            <main>
+                <section>
+                    <h2>{ "Today is: 2024-07-27" }</h2>
+                    <article>
+                        <p>{ "Site compiled to WebAssembly, using the Rust frontend framework called Yew" }</p>
+                    </article>
+                </section>
+            </main>
+            <footer>
+            <a href="https://yew.rs" target="_blank" rel="noreferrer">{ "Learn more about Yew" }</a>
+            </footer>
+        </>
     }
 }
 
 // main function, renders the app component to the screen
 fn main() {
+    // initialize console error panic hook
+    console_error_panic_hook::set_once();
+
+    // initialize wasm logger
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+
+    // render the main app
     yew::Renderer::<App>::new().render();
 }
